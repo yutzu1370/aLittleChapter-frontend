ca50809079bb49f3de4edb143b53f2dc94dfa860
yutzu1370 <doim1370523@gmail.com>
2025-06-23 15:52:27 +0800

    更改公告內容

--- components/home/AnnouncementBanner.tsx
+++ components/home/AnnouncementBanner.tsx
@@ -4,7 +4,7 @@ export default function AnnouncementBanner() {
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
         <div className="overflow-hidden">
           <div className="whitespace-nowrap animate-marquee">
-            <span className="text-white inline-block text-xs sm:text-sm lg:text-base">【出貨延遲】遇連假1/25~2/2出貨將順延，敬請見諒。</span>
+            <span className="text-white inline-block text-xs sm:text-sm lg:text-base">【出貨延遲】遇公司全體員工出國旅遊7/10~7/15，故出貨將順延，敬請見諒。</span>
           </div>
         </div>
       </div>
